6cbda8c93e4dda2f6e34ae23b92b3d906159f851
seeleng <seelengxd@gmail.com>
2024-09-13 16:07:29 +0800

    feat: add run_request function
    very primitive

--- unicon_runner/app.py
+++ unicon_runner/app.py
@@ -1,12 +1,21 @@
 from enum import Enum
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from unicon_runner.executor.run import run_request
+import os
 
 from unicon_runner.schemas import Request
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    if "temp" not in os.listdir():
+        os.mkdir("temp")
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/submissions")
 def run_submission(request: Request):
-    return "success"
+    return run_request(request)
---
+++ unicon_runner/executor/run.py
@@ -0,0 +1,41 @@
+from unicon_runner.schemas import Request
+import subprocess
+from uuid import uuid4
+import os
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+env = Environment(loader=FileSystemLoader("unicon_runner/executor/templates"),
+                  autoescape=select_autoescape())
+dockerfile_template = env.get_template("Dockerfile.jinja")
+
+
+def run_request(request: Request):
+    # 1. Generate temp folder name
+    folder_name = str(uuid4()).replace("-", "")
+    folder_path = os.path.join("temp", folder_name)
+    os.mkdir(folder_path)
+
+    # 2. Create files
+    for file in request.files:
+        with open(os.path.join(folder_path, file.file_name), "w") as f:
+            f.write(file.content)
+
+    with open(os.path.join(folder_path, "Dockerfile"), "w") as f:
+        dockerfile = dockerfile_template.render(entrypoint=request.entrypoint)
+        f.write(dockerfile)
+
+    # 3. Spawn podman container
+    result = subprocess.run(["podman", "build", os.path.join(
+        folder_path), "-t", folder_name], capture_output=True, text=True)
+
+    result = subprocess.run(["podman", "run", "--name", folder_name + "_run", folder_name],
+                            capture_output=True, text=True)
+
+    # 4. Output raw result
+    stdout = result.stdout
+    stderr = result.stderr
+
+    return {
+        "stdout": stdout,
+        "stderr": stderr
+    }
