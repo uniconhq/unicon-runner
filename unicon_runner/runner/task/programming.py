a034504701637c69c28b06a708c4b61b29f76f84
seeleng <seelengxd@gmail.com>
2024-10-13 13:47:04 +0800

    feat: add code generation method instead of running separately

--- unicon_runner/runner/task/programming.py
+++ unicon_runner/runner/task/programming.py
@@ -59,12 +59,26 @@ class Step(
     ) -> dict:
         pass
 
+    def get_comment_header(self):
+        return f"# Step {self.id}: {self.type.value}"
+
+    @abc.abstractmethod
+    def get_code(self, inputs: dict):
+        pass
+
 
 class ConstantStep(Step[Unused, Unused, dict]):
     values: dict
 
     async def run(self, _: dict, *__unused_args):
         return self.values
+
+    def get_code(self, *__unused_args):
+        code = [self.get_comment_header()]
+        for output in self.outputs:
+            value = self.values[output.name]
+            code.append(f"var_{self.id}_{output.id} = {value}")
+        return "\n".join(code)
 
 
 class InputStep(Step[Unused, Unused, dict]):
@@ -73,10 +87,28 @@ class InputStep(Step[Unused, Unused, dict]):
     async def run(self, *__unused_args):
         return self.values
 
+    def get_code(self, *__unused_args):
+        code = [self.get_comment_header()]
+        for output in self.outputs:
+            value = self.values[output.name]
+            code.append(f"var_{self.id}_{output.id} = {value}")
+        return "\n".join(code)
+
 
 class OutputStep(Step[dict, Unused, dict]):
     async def run(self, params: dict, *__unused_args):
         return params
+
+    def get_code(self, inputs: dict):
+        code = [self.get_comment_header()]
+        result = (
+            "{"
+            + ", ".join((f'"{key}": {variable_name}' for key, variable_name in inputs.items()))
+            + "}"
+        )
+        print_statement = f"print({result})"
+        code.append(print_statement)
+        return "\n".join(code)
 
 
 class StringMatchStep(Step[str, str, bool]):
@@ -90,6 +122,15 @@ class StringMatchStep(Step[str, str, bool]):
 
     async def _run(self, input: str, expected_answer: str, *__unused_args) -> bool:
         return input == expected_answer
+
+    def get_code(self, inputs: dict):
+        output = self.outputs[0]
+        output_variable = f"var_{self.id}_{output.id}"
+        input_variables = list(inputs.values())
+        assert len(input_variables) == 2
+        code = [self.get_comment_header()]
+        code.append(f"{output_variable} = str({input_variables[0]}) == str({input_variables[1]})")
+        return "\n".join(code)
 
 
 class Params(BaseModel):
@@ -145,6 +186,23 @@ class PyRunFunctionStep(Step[list[File], Unused, ExecutorResult]):
             request_id=str(uuid4()),
         )
 
+    def get_code(self, inputs: dict):
+        # Precondition: set_user_input has been called
+        assert self.user_input != None
+
+        if not any(f.file_name == self.file_name for f in self.user_input):
+            raise ValueError(f"File {self.file_name} not found in input files")
+
+        assert len(inputs) == 1
+        params_variable = list(inputs.values())[0]
+
+        func_invocation = f"{self.function_name}(*{params_variable}['arguments'], **{params_variable}['keyword_arguments'])"
+        # TODO: Remove dependence on `print` and `stdout`
+        output = self.outputs[0]
+        output_variable = f"var_{self.id}_{output.id}"
+        assembled_code = f"from {self.file_name.split(".py")[0]} import {self.function_name}\n\n{output_variable} = {func_invocation}"
+        return self.get_comment_header() + "\n" + assembled_code
+
 
 class ProgrammingTaskExpectedAnswer(BaseModel):
     testcase_id: int
@@ -160,15 +218,13 @@ class Link(BaseModel):
     to_socket_id: int
 
 
-@dataclass
-class Graph:
+class Graph(BaseModel):
     steps: list[Step]
     links: list[Link]
-    user_input: list[File]
-    environment: ProgrammingEnvironment
-    executor: Executor
 
-    async def run(self) -> dict:
+    async def run(
+        self, user_input: list[File], environment: ProgrammingEnvironment, executor: Executor
+    ) -> dict:
         # for lookup
         step_map = {step.id: step for step in self.steps}
         link_map = {
@@ -184,6 +240,8 @@ class Graph:
             if not step.inputs:
                 ready_queue.append((step, {}))
 
+        script_parts = []
+
         while True:
             if not ready_queue:
                 raise ValueError("Ended without reaching output node.")
@@ -191,22 +249,17 @@ class Graph:
             current_step, inputs = ready_queue.popleft()
 
             if current_step.type == StepType.PY_RUN_FUNCTION:
-                current_step.set_user_input(self.user_input)
+                current_step.set_user_input(user_input)
 
-            outputs = await current_step.run(inputs, self.environment, self.executor)
+            script_part = current_step.get_code(inputs)
+            script_parts.append(script_part)
 
             if current_step.type == StepType.OUTPUT:
                 break
 
             # update forming map
             for outgoing_link in link_map[current_step.id]:
-                outgoing_key = [
-                    output
-                    for output in current_step.outputs
-                    if output.id == outgoing_link.from_socket_id
-                ][0].name
-                value = outputs[outgoing_key]
-                print(value)
+                value = f"var_{outgoing_link.from_node_id}_{outgoing_link.from_socket_id}"
 
                 if not forming_map.get(outgoing_link.to_node_id):
                     ingoing_step = step_map[outgoing_link.to_node_id]
@@ -225,7 +278,36 @@ class Graph:
                     ready_queue.append((step, params))
                     del forming_map[id]
 
-        return outputs
+        script = "\n\n".join(script_parts)
+
+        return await executor.run_request(
+            request=Request(
+                files=user_input + [File(file_name="__run.py", content=script)],
+                environment=environment,
+                entrypoint="__run.py",
+            ),
+            request_id=str(uuid4()),
+        )
+
+
+# class LoopStep(Step):
+#     subgraph: Graph
+
+#     def set_user_input(self, user_input: list[File]):
+#         self.user_input = user_input
+
+#     async def run(
+#         self,
+#         inputs: dict,
+#         environment: ProgrammingEnvironment,
+#         executor: Executor,
+#     ):
+#         state = inputs.get("state")
+#         while True:
+#             outputs = await self.subgraph.run(state, environment, executor)
+#             if outputs["end"]:
+#                 break
+#         return outputs
 
 
 class Testcase(BaseModel):
@@ -246,8 +328,8 @@ class Testcase(BaseModel):
             for step_expected_answer in expected_answer
         }
 
-        graph = Graph(self.steps, self.links, user_input, environment, executor)
-        return await graph.run()
+        graph = Graph.model_validate({"steps": self.steps, "links": self.links})
+        return await graph.run(user_input, environment, executor)
 
 
 class TaskType(str, Enum):
