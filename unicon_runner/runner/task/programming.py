692de0684df97019bb7d203de5e78da35e13321d
seeleng <seelengxd@gmail.com>
2024-10-15 13:21:04 +0800

    feat: current loop progress
    not done, but added the todos for breaking condition step

--- unicon_runner/runner/task/programming.py
+++ unicon_runner/runner/task/programming.py
@@ -1,5 +1,5 @@
 import abc
-from collections import defaultdict
+from collections import defaultdict, deque
 from enum import Enum
 from itertools import groupby
 from typing import Any, Generic, TypeVar
@@ -26,6 +26,8 @@ class StepType(str, Enum):
     CONSTANT = "CONSTANT_STEP"
     EXTRACT_PROGRAM_OUTPUT = "EXTRACT_PROGRAM_OUTPUT_STEP"
     PARAMS_WITHOUT_KEYWORD_ARGS = "PARAMS_WITHOUT_KEYWORD_ARGS_STEP"
+    SUBGRAPH_INIT = "SUBGRAPH_INIT_STEP"
+    BREAKING_CONDITION = "BREAKING_CONDITION_STEP"
 
 
 StepInputType = TypeVar("StepInputType")
@@ -178,24 +180,167 @@ class Link(BaseModel):
     to_socket_id: int
 
 
-# class LoopStep(Step):
-#     subgraph: Graph
+class Graph(BaseModel):
+    steps: list[Step]
+    links: list[Link]
 
-#     def set_user_input(self, user_input: list[File]):
-#         self.user_input = user_input
+    def get_code(self, user_input: list[File], inputs: dict):
+        # TODO: if inputs is not empty, look for the first step (it better be a subgraph init node)
+        # set the value to state variable!
 
-#     async def run(
-#         self,
-#         inputs: dict,
-#         environment: ProgrammingEnvironment,
-#         executor: Executor,
-#     ):
-#         state = inputs.get("state")
-#         while True:
-#             outputs = await self.subgraph.run(state, environment, executor)
-#             if outputs["end"]:
-#                 break
-#         return outputs
+        # for lookup
+        step_map = {step.id: step for step in self.steps}
+        link_map = {
+            id: [link for link in self.links if link.from_node_id == id]
+            for id in set(link.from_node_id for link in self.links)
+        }
+
+        ready_queue = deque[tuple[Step, dict]]()
+        forming_map = {}
+
+        # initialisation step: move all nodes with no inputs to ready queue
+        for step in self.steps:
+            if not step.inputs:
+                ready_queue.append((step, {}))
+
+        script_parts = []
+
+        while True:
+            if not ready_queue:
+                raise ValueError("Ended without reaching output node.")
+
+            current_step, inputs = ready_queue.popleft()
+
+            if current_step.type == StepType.PY_RUN_FUNCTION:
+                current_step.set_user_input(user_input)
+
+            script_part = current_step.get_code(inputs)
+            script_parts.append(script_part)
+
+            if current_step.type == StepType.OUTPUT:
+                break
+
+            # update forming map
+            for outgoing_link in link_map[current_step.id]:
+                value = f"var_{outgoing_link.from_node_id}_{outgoing_link.from_socket_id}"
+
+                if not forming_map.get(outgoing_link.to_node_id):
+                    ingoing_step = step_map[outgoing_link.to_node_id]
+                    forming_map[outgoing_link.to_node_id] = (ingoing_step, {})
+
+                ingoing_step = step_map[outgoing_link.to_node_id]
+                ingoing_key = [
+                    input for input in ingoing_step.inputs if input.id == outgoing_link.to_socket_id
+                ][0].name
+
+                forming_map[outgoing_link.to_node_id][1][ingoing_key] = value
+
+            # move formed nodes to ready_queue
+            for id, (step, params) in list(forming_map.items()):
+                if len(step.inputs) == len(params):
+                    ready_queue.append((step, params))
+                    del forming_map[id]
+
+        script = "\n\n".join(script_parts)
+        return script
+
+    async def run(
+        self, user_input: list[File], environment: ProgrammingEnvironment, executor: Executor
+    ) -> dict:
+        script = self.get_code(user_input, {})
+        return await executor.run_request(
+            request=Request(
+                files=user_input + [File(file_name="__run.py", content=script)],
+                environment=environment,
+                entrypoint="__run.py",
+            ),
+            request_id=str(uuid4()),
+        )
+
+
+class SubgraphInitStep(Step):
+    """
+    This node's purpose is to allow the subgraph to set the state variable
+    so that the state in subgraph.run() can properly propagate to the other nodes.
+    """
+
+    # TODO: implement
+    state_variable: str | None = None
+
+    def set_state_variable(self, state_variable: str):
+        self.state_varaible = state_variable
+
+    def get_code(self):
+        assert self.state_variable is not None
+        assert len(self.outputs) == 1
+
+        output = self.outputs[0]
+        output_variable = f"var_{self.id}_{output.id}"
+        return self.get_comment_header() + "\n" + f"{output_variable} = {self.state_varaible}"
+
+
+class BreakingConditionStep(Step):
+    """This should output:
+
+    def <breaking_condition>():
+        ...
+
+    if <breaking_condition>(state):
+        break
+    """
+
+    # TODO: implement
+    pass
+
+
+class LoopStep(Step):
+    subgraph: Graph
+    breaking_condition: BreakingConditionStep
+    user_input: list[File] | None = None
+
+    def set_user_input(self, user_input: list[File]):
+        self.user_input = user_input
+
+    def get_code(self, inputs: dict):
+        assert self.user_input is not None
+
+        code = [self.get_comment_header()]
+
+        # First, define a state variable. set it to inputs, there should be one
+        input_variables = list(inputs.values())
+        assert len(input_variables) == 1
+        state_variable = f"var_{self.id}_state"
+        code.append(f"{state_variable} = {input_variables[0]}")
+
+        # while True
+        code.append("while True:")
+
+        # call breaking condition
+        code.append(self.breaking_condition.get_code({"state": state_variable}))
+
+        # pass in the subgraph
+        ## first pass state_variable to loopinit node
+        assert self.subgraph.steps[0].type == StepType.SUBGRAPH_INIT
+        subgraph_init_step: SubgraphInitStep = self.subgraph.steps[0]
+        subgraph_init_step.set_state_variable(state_variable)
+
+        ## get subgraph code
+        subgraph_code = self.subgraph.get_code(self.user_input)
+
+        ## indent subgraph code
+        subgraph_code.replace("\n", "\n\t")
+
+        # TODO: find output variable of output step
+        subgraph_output_steps = [
+            step for step in self.subgraph.steps if step.type == StepType.OUTPUT
+        ]
+        assert len(subgraph_output_steps) == 1
+        subgraph_output_step = subgraph_output_steps[0]
+
+        subgraph_output_variable = f"var_{subgraph_output_step.outputs[0].id}_1"
+
+        # set state to this variable
+        subgraph_code.append(f"\t{state_variable} = {subgraph_output_variable}")
 
 
 class Testcase(BaseModel):
