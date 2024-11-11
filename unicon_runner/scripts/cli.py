be0c42afd53eaeaa7aca6f61295ac5f8c5c023aa
seeleng <seelengxd@gmail.com>
2024-11-11 22:25:07 +0800

    fix: cli script

--- unicon_runner/scripts/cli.py
+++ unicon_runner/scripts/cli.py
@@ -5,7 +5,6 @@ from pprint import pprint
 
 from unicon_runner.lib.constants import RUNNER_TYPE
 from unicon_runner.runner.runner import Runner, RunnerType
-from unicon_runner.runner.task.programming import ProgrammingTask
 from unicon_runner.schemas import Request
 
 with open("unicon_runner/scripts/test2.json") as f:
@@ -16,10 +15,10 @@ executor = Runner(RunnerType(RUNNER_TYPE))
 
 async def run_programming_task():
     task = EXAMPLE
-    result = await executor.run_request(Request())
-    result = await executor.run_programming_task(
-        programming_task=ProgrammingTask.model_validate_json(task)
-    )
+    result = await executor.run_request(Request.model_validate_json(task))
+    # result = await executor.run_programming_task(
+    #     programming_task=ProgrammingTask.model_validate_json(task)
+    # )
     message = result.model_dump()
     pprint(message)
 
