2f1560bfbc1c73a557ab396bc4384b4b4302837e
yihong <shenyihong@u.nus.edu>
2025-01-04 00:31:10 +0800

    feat: implement `conty` NVIDIA GPU support (#18)
    * fix: disable `conty` custom NVIDIA driver installation
    
    * chore: pin python version to 3.12
    
    * feat: `bwrap` flags for GPU access
    
    * fix: missing DEST for working directory bind mount
    
    * fix: R/W perms for `uv` app state and cache path
    
    * refactor: update `uv_path` based on `v0.5` releaase
    
    Co-authored-by: see leng <34371483+seelengxd@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: see leng <34371483+seelengxd@users.noreply.github.com>

--- unicon_runner/executor/sandbox.py
+++ unicon_runner/executor/sandbox.py
@@ -26,30 +26,37 @@ class SandboxExecutor(UnsafeExecutor):
     def _cmd(self, cwd: Path) -> tuple[list[str], dict[str, str]]:
         assert CONTY_PATH is not None
 
-        # NOTE: `uv` binary is assumed to be stored under `~/.cargo/bin/`
+        # NOTE: `uv` binary is assumed to be stored under `~/.local/bin/`
         # We are using `uv` as the environment manager and program runner
-        uv_path = Path("~/.cargo/bin/uv").expanduser()
-        # NOTE: We need to bind the uv cache folder to access uv-managed python executables
-        uv_cache_path = Path("~/.local/share/uv").expanduser()
+        uv_path = Path("~/.local/bin/uv").expanduser()
+        # NOTE: We need to bind the uv app state folder to access uv-managed python executables
+        uv_app_state_path = Path("~/.local/share/uv").expanduser()
+        uv_cache_path = Path("~/.cache/uv").expanduser()
 
+        # fmt: off
         return [
             CONTY_PATH,
-            "--bind",
-            str(cwd.absolute()),
-            str(cwd),
-            "--ro-bind",
-            *([str(uv_path)] * 2),
-            "--ro-bind",
-            *([str(uv_cache_path)] * 2),
+            "--ro-bind", *(["/"] * 2),
+            "--ro-bind", *([str(uv_path)] * 2),
+            "--ro-bind", *([str(uv_app_state_path)] * 2),
+            "--bind", *([str(uv_cache_path)] * 2),
+            # R/W bind to the root working directory
+            "--bind", *([str(cwd.parents[0])] * 2),
+            # NOTE: Mount `procfs` to allow access to process information
+            # This seems be required for GPU workloads
+            "--proc", "/proc",
+            # Bind /dev to allow access to devices
+            "--dev-bind", *(["/dev"] * 2),
             str(cwd / self.ENTRYPOINT),
         ], {
             # Conty specific environment variables
-            "SANDBOX": "1",
-            "SANDBOX_LEVEL": "1",
-            "QUIET_MODE": "1",
+            "SANDBOX": "1", "SANDBOX_LEVEL": "1", "QUIET_MODE": "1",
+            # NOTE: `conty` installs NVIDIA drivers in the sandbox, so we need to disable it
+            "NVIDIA_HANDLER": "-1",
             # NOTE: We need to unset VIRTUAL_ENV to prevent uv from using the wrong base python interpreter
             "VIRTUAL_ENV": "''",
         }
+        # fmt: off
 
     async def _collect(self, proc: asyncio.subprocess.Process) -> ExecutorResult:
         stdout, stderr = await proc.communicate()
